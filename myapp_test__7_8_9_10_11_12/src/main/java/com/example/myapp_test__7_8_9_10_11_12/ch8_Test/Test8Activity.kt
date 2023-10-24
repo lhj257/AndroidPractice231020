67f2d14ded5c034e3851471a6c4e4571f5649d4b
lhj257 <139347455+lhj257@users.noreply.github.com>
2023-10-24 17:05:28 +0900

    2310223 수업(이벤트)

--- myapp_test__7_8_9_10_11_12/src/main/java/com/example/myapp_test__7_8_9_10_11_12/ch8_Test/Test8Activity.kt
+++ myapp_test__7_8_9_10_11_12/src/main/java/com/example/myapp_test__7_8_9_10_11_12/ch8_Test/Test8Activity.kt
@@ -2,6 +2,7 @@ package com.example.myapp_test__7_8_9_10_11_12.ch8_Test
 
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
 import android.view.MotionEvent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -20,6 +21,18 @@ class Test8Activity : AppCompatActivity() {
         //모두 담은 인스턴스의 부모 레이아웃을 출력을 함.
         setContentView(activityTest8Binding.root)
 
+        //test 버튼 클릭 리스너 이벤트 처리 SAM 기법(Sing Abstract Method),
+        //보통 자바에서, 람다식을 연결할때 사용하는 인터페이스
+        //함수형 인터페이스, 추상 메서드가 하나인 인터페이스
+        activityTest8Binding.testBtn.setOnClickListener {
+            Toast.makeText(this@Test8Activity,"test!!!!!",Toast.LENGTH_SHORT).show()
+         }
+
+        //롱 클릭 이벤트 추가하기.
+        activityTest8Binding.longBtn.setOnLongClickListener {
+            Toast.makeText(this@Test8Activity,"Long Clicked!!!!!",Toast.LENGTH_SHORT).show()
+            true
+        }
 
     }
     override fun onTouchEvent(event: MotionEvent): Boolean{
@@ -37,6 +50,15 @@ class Test8Activity : AppCompatActivity() {
             }
         }
         return super.onTouchEvent(event)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        when(keyCode){
+            KeyEvent.KEYCODE_0 -> Log.d(TAG,"0키누름")
+            KeyEvent.KEYCODE_A -> Log.d(TAG,"A키누름")
+            KeyEvent.KEYCODE_ENTER -> Log.d(TAG,"ENTER키누름")
+        }
+        return super.onKeyDown(keyCode, event)
     }
 
 }
