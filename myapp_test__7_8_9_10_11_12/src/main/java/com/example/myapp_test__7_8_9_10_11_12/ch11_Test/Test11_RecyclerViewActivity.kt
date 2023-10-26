3b8959f28d44b02e6a3c1adc7c65df3ea60508dd
lhj257 <139347455+lhj257@users.noreply.github.com>
2023-10-26 16:33:02 +0900

    2310226 7교시

---
+++ myapp_test__7_8_9_10_11_12/src/main/java/com/example/myapp_test__7_8_9_10_11_12/ch11_Test/Test11_RecyclerViewActivity.kt
@@ -0,0 +1,39 @@
+package com.example.myapp_test__7_8_9_10_11_12.ch11_Test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapp_test__7_8_9_10_11_12.ch11_Test.recycler.MyAdapter
+import com.example.myapp_test__7_8_9_10_11_12.databinding.ActivityTest11RecyclerViewBinding
+
+class Test11_RecyclerViewActivity : AppCompatActivity() {
+    lateinit var binding : ActivityTest11RecyclerViewBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTest11RecyclerViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val datas= mutableListOf<String>()
+        for(i in 1..10){
+            datas.add("올빼미 $i")
+        }
+        //기본 값으로 세로 방향 출력.
+//        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        //가로 방향으로 출력 해보기.
+        val layoutManager=LinearLayoutManager(this)
+        layoutManager.orientation= LinearLayoutManager.HORIZONTAL
+        binding.recyclerView.layoutManager= layoutManager
+        binding.recyclerView.adapter = MyAdapter(datas)
+        // 구분선 넣기, 나중에 옵션으로 배경이미지도 넣기 가능.
+        binding.recyclerView.addItemDecoration(DividerItemDecoration(this,LinearLayoutManager.VERTICAL))
+
+        //2번째 리사이클러 뷰 출력해보기. 기존 데이터 재사용, 뷰홀더, 마이 어댑터 재사용해보기.
+        binding.recyclerView2.layoutManager=LinearLayoutManager(this)
+        binding.recyclerView2.adapter = MyAdapter(datas)
+        // 구분선 넣기, 나중에 옵션으로 배경이미지도 넣기 가능.
+        binding.recyclerView2.addItemDecoration(DividerItemDecoration(this,LinearLayoutManager.VERTICAL))
+
+
+    }
+}
