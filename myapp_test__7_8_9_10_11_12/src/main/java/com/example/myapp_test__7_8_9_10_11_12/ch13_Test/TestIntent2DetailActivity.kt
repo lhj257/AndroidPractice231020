ed7ab9af4c237d9efcdfc53f5f19f6a206618fea
lhj257 <139347455+lhj257@users.noreply.github.com>
2023-10-31 16:36:52 +0900

    2310231 7교시

--- myapp_test__7_8_9_10_11_12/src/main/java/com/example/myapp_test__7_8_9_10_11_12/ch13_Test/TestIntent2DetailActivity.kt
+++ myapp_test__7_8_9_10_11_12/src/main/java/com/example/myapp_test__7_8_9_10_11_12/ch13_Test/TestIntent2DetailActivity.kt
@@ -1,26 +1,77 @@
 package com.example.myapp_test__7_8_9_10_11_12.ch13_Test
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapp_test__7_8_9_10_11_12.databinding.ActivityTestIntent2DetailBinding
 
 class TestIntent2DetailActivity : AppCompatActivity() {
-    lateinit var binding:ActivityTestIntent2DetailBinding
-    //2번 화면
+    lateinit var binding :ActivityTestIntent2DetailBinding
+    // 2번 화면
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("lhj","2번 onCreate()")
         binding = ActivityTestIntent2DetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //1번 화면에서 넘어온 데이터 받아서, 결과 뷰에 붙이기 작업.
+        // 1번 화면에서 넘어온 데이터 받아서, 결과 뷰에 붙이기 작업.
         // 임시 저장소에 담기.
-        val name=intent.getStringExtra("name")
-        val email=intent.getStringExtra("email")
-        val age=intent.getIntExtra("age",28)
+        val email = intent.getStringExtra("email")
+        val password = intent.getStringExtra("password")
+        val age = intent.getStringExtra("age")
+//        val age = intent.getIntExtra("age",30)
 
-        binding.resultNameView.text="이름 : $name"
-        binding.resultEmailView.text="이메일 : $email"
-        binding.resultAgeView.text="나이 : $age"
+        // 데이터 확인
+        Log.d("lhj","데이터 확인 2번 화면 이메일: ${email} , " +
+                "패스워드: ${password}, 나이: ${age}")
 
+        binding.resultNameView.text = "이메일 : $email"
+        binding.resultEmailView.text = "패스워드 : $password"
+        binding.resultAgeView.text = "나이 : $age "
+
+        //후처리 데이터 보내기 작업.
+        binding.TestBtn3.setOnClickListener {
+            //메세지 담기
+            intent.putExtra("resultData","2번 화면에서 데이터 가져온 값.")
+            //결과 코드 담기.
+            setResult(RESULT_OK,intent)
+            //현재 앱을 종료하는 함수 -> 루트 액티비티가 아니면, fisnish() 호출시, 모두 종료
+            // 예) 1번 화면(루트 액티비티)에서 -> 2번화면(비루트 액티비티)으로 이동시
+            finish()
+        }
+        //후처리 데이터 보내기 방법2 작업.
+        binding.TestBtn4.setOnClickListener {
+            //메세지 담기
+            intent.putExtra("result","2번 화면에서 데이터 가져온 값.")
+            //결과 코드 담기.
+            setResult(RESULT_OK,intent)
+            //현재 앱을 종료하는 함수 -> 루트 액티비티가 아니면, fisnish() 호출시, 모두 종료
+            // 예) 1번 화면(루트 액티비티)에서 -> 2번화면(비루트 액티비티)으로 이동시
+            finish()
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d("lhj","2번 onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("lhj","2번 onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("lhj","2번 onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("lhj","2번 onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("lhj","2번 onDestroy()")
     }
 }
--- myapp_test__7_8_9_10_11_12/src/main/java/com/example/myapp_test__7_8_9_10_11_12/ch13_Test/TestIntentActivity.kt
+++ myapp_test__7_8_9_10_11_12/src/main/java/com/example/myapp_test__7_8_9_10_11_12/ch13_Test/TestIntentActivity.kt
@@ -1,27 +1,149 @@
 package com.example.myapp_test__7_8_9_10_11_12.ch13_Test
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapp_test__7_8_9_10_11_12.databinding.ActivityTestIntentBinding
 
 class TestIntentActivity : AppCompatActivity() {
-    //1번 화면
-    lateinit var binding: ActivityTestIntentBinding
+    // 1번 화면
+    lateinit var binding : ActivityTestIntentBinding
+    //    lateinit var email : String
+//    lateinit var password : String
+//    lateinit var age : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityTestIntentBinding.inflate(layoutInflater)
+        Log.d("lhj","onCreate()")
+        binding = ActivityTestIntentBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //인텐트에 기본 데이터 추가 및 가져오기 테스트.
-        binding.testBtn1.setOnClickListener {
-            //데이터를 추가해서 + 화면 이동 : 내부 앱끼리 연동
-            val intent = Intent(this@TestIntentActivity,TestIntent2DetailActivity::class.java)
-            //데이터 추가 해보기.
-            intent.putExtra("name","lhj")
-            intent.putExtra("email","lhj@naver.com")
-            intent.putExtra("age",20)
+
+        // 인텐트에 기본 데이터 추가 및 가져오기 테스트.
+        binding.testBtn2.setOnClickListener {
+            // 데이터를 추가해서 + 화면 이동. : 내부 앱끼리 연동
+            val intent = Intent(this@TestIntentActivity,
+                TestIntent2DetailActivity::class.java)
+
+            // 위에서 lateinit var 선언을 하면, 화면이 최초 그려질 때,
+            // 입력이 안된 상태로 할당이 되어 버림.
+            // 그래서, 입력 란에 입력 후, 그 시점에 있는 값을
+            // 지금 , 버튼을 눌려서 전달을 하기.
+            val email = binding.emailEdit.text.toString()
+            val password = binding.passwordEdit.text.toString()
+            val age = binding.ageEdit.text.toString()
+
+            // 데이터 추가 해보기.
+            intent.putExtra("email",email)
+            intent.putExtra("password",password)
+            intent.putExtra("age",age)
             startActivity(intent)
+
         }
+
+        //후처리, 1번 화면에서 -> 2번 화면으로 이동후 -> 특정 액션
+        // -> 다시 1번 화면 으로 데이터 가져오기
+        //예) 1번화면 -> 2번화면(갤러리) -> 2번에서 사진 선택 후 ->1번으로 가져오기.
+        binding.testBtn.setOnClickListener {
+            val intent: Intent = Intent(this@TestIntentActivity,TestIntent2DetailActivity::class.java)
+            //데이터 보내기 했음.
+//            intent.putExtra("email","lhj)
+//            intent.putExtra("password","1234")
+            //후처리 하기
+            //10 : 요청 코드, 식별하기 위한 식별자.
+            startActivityForResult(intent,10)
+            //2번 화면으로 넘어감.
+        }
+
+        // 후처리 함수 정의하기.
+        //registerForActivityResult -> StartActivityForResult, 시스템에서 정의해둔 함수
+        //2번 화면에서 데이터를 가져왔을 때, 처리하는 함수.
+        val requestLauncher : ActivityResultLauncher<Intent> = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            val result2 =it.data?.getStringExtra("result")
+            binding.resultData1.text="결과 result2 : $result2"
+        }
+
+        //후처리 데이터 가져오기 방법2
+        binding.testBtn3.setOnClickListener {
+            val intent: Intent = Intent(this@TestIntentActivity,TestIntent2DetailActivity::class.java)
+
+            //
+            requestLauncher.launch(intent)
+            //2번 화면으로 넘어감.
+        }
+
+
+        //인텐트 필터를 이용해서 외부앱에 접근 하기.
+        // 인텐트 : 시스템에 전달하는 메세지
+        //의 속성에 intent-filter : 무엇을 하는지를 나타내는 라벨링.
+        // 시스템이 해달 필터의 요소를 보고서 해당외부앱을 연결 시켜줌.
+        //예) 좌표값이다. 위도 경고-> 지도 앱(외부앱)
+//        예2) http 주소다. -> 웹 브라우저 앱(외부앱)
+        binding.testBtn4.setOnClickListener {
+            //지도 맵 열기 테스트.
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:37.7749,127.4194"))
+            //현재, 지도 관련 앱이 다양하게 없어서, 아마도 기본 앱: 구글 맵이 나옴.
+            //만약, 지도 관련 앱이 여러 개 있다면, 특정 앱을 선택이 가능함.
+            // 사용하는 앱의 패키지명을 정확히 입력함.
+            intent.setPackage("com.google.android.apps.maps")
+            startActivity(intent)
+
+        }
+//크롬 외부앱 테스트
+        binding.testBtn5.setOnClickListener {
+            //지도 맵 열기 테스트.
+            val intent = Intent()
+            intent.action = Intent.ACTION_VIEW
+            intent.data=Uri.parse("http://www.naver.com")
+            //현재, 지도 관련 앱이 다양하게 없어서, 아마도 기본 앱: 구글 맵이 나옴.
+            //만약, 지도 관련 앱이 여러 개 있다면, 특정 앱을 선택이 가능함.
+            // 사용하는 앱의 패키지명을 정확히 입력함.
+            startActivity(intent)
+
+        }
+
+        //onCreate 마지막
+    }
+    //onCreate 밖에서 재정의하기.
+    //2번 화면에서 넘어온 데이터를 받아서, 처리 하는 함
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //자동완성 된 코드에 필요한 로직만 추가하기.
+        //2번 화면에서, resultCode를 설정하고,
+        //넘어온 데이터는 data에 담아져 있다.
+        if(requestCode==10 && resultCode== Activity.RESULT_OK){
+            val result = data?.getStringExtra("resultData")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("lhj","onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("lhj","onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("lhj","onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("lhj","onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("lhj","onDestroy()")
     }
 }
