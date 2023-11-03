ad5410de0e32d0ab50182531b5ef5c0ebbf3d0c8
lhj257 <139347455+lhj257@users.noreply.github.com>
2023-11-03 10:19:27 +0900

    20231103 2교시ing

--- myapp_test__7_8_9_10_11_12/src/main/java/com/example/myapp_test__7_8_9_10_11_12/ch17_Test/PreferenceTest/SharedPrefDetailTestActivity.kt
+++ myapp_test__7_8_9_10_11_12/src/main/java/com/example/myapp_test__7_8_9_10_11_12/ch17_Test/PreferenceTest/SharedPrefDetailTestActivity.kt
@@ -1,14 +1,32 @@
 package com.example.myapp_test__7_8_9_10_11_12.ch17_Test.PreferenceTest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.myapp_test__7_8_9_10_11_12.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapp_test__7_8_9_10_11_12.databinding.ActivitySharedPrefDetailTestBinding
+
 
 class SharedPrefDetailTestActivity : AppCompatActivity() {
     // 2번 화면
     // 공유프리퍼런스 파일에 데이터 가져오기
+    lateinit var binding : ActivitySharedPrefDetailTestBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shared_pref_detail_test)
+        binding = ActivitySharedPrefDetailTestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //조회 버튼 누르면 공유된 프리퍼런스 파일에 저장된 값 가져오기.
+        binding.getSharedPreferBtnTest.setOnClickListener {
+            // 값을 가져오기
+            // 저장된 파일명 : userInfo
+            //key, value 형태로 저장됨
+            // key로 해당 값을 불러오기
+            val pref = getSharedPreferences("userInfo", MODE_PRIVATE)
+            val email = pref.getString("email", "Default EMAIL")
+            val password = pref.getString("password", "Default PASSWORD")
+
+            //결과 뷰에 데이터 넣기
+            binding.resultEmailSP.text=email
+            binding.resultPasswordSP.text = password
+        }
     }
 }
